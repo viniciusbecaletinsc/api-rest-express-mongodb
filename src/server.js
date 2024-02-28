ae4d602c0113da40ff9b74a9d88bfeb29185c0d5
Vinicius Becaleti <vinicius.becaleti@gruponsc.com.br>
2024-02-28 20:24:33 -0300

    feat: Adiciona rota /livros para retornar o MOCK_LIVROS

--- src/server.js
+++ src/server.js
@@ -3,8 +3,23 @@ import express from 'express'
 const app = express()
 const port = 3000
 
+const MOCK_LIVROS = [
+  {
+    id: 1,
+    title: "O Senhor dos Anéis"
+  },
+  {
+    id: 2,
+    title: "Harry Potter"
+  }
+]
+
 app.get("/", (req, res) => {
-  res.send("Hello World")
+  return res.send("Hello World")
+})
+
+app.get("/livros", (req, res) => {
+  return res.json(MOCK_LIVROS)
 })
 
 app.listen(port, () => console.log(`Server is running on port ${port}`))

6760f58b115f5d01ee35deb21b5bd84e2ef6a621
Vinicius Becaleti <vinicius.becaleti@gruponsc.com.br>
2024-02-29 17:28:41 -0300

    refact: altera as rotas do bookRoutes e adiciona prefixo na routes global

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -4,7 +4,7 @@ import { BookController } from '../controllers/BookController.js'
 export const bookRoutes = Router()
 const bookController = new BookController()
 
-bookRoutes.get("/livros", bookController.index)
-bookRoutes.post("/livros", bookController.store)
-bookRoutes.put("/livros/:id", bookController.update)
-bookRoutes.delete("/livros/:id", bookController.delete)
+bookRoutes.get("/", bookController.index)
+bookRoutes.post("/", bookController.store)
+bookRoutes.put("/:id", bookController.update)
+bookRoutes.delete("/:id", bookController.delete)

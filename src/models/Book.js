42b15b83e0822b9f01add3270aa16ec52da1c0aa
Vinicius Becaleti <vinicius.becaleti@gruponsc.com.br>
2024-02-29 15:19:48 -0300

    feat: adiciona o model Book

---
+++ src/models/Book.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose'
+
+const bookSchema = new mongoose.Schema({
+  id: {
+    type: mongoose.Schema.Types.ObjectId
+  },
+  title: {
+    type: mongoose.Schema.Types.String,
+    required: true
+  },
+  author: {
+    type: mongoose.Schema.Types.String,
+    required: true
+  },
+  price: {
+    type: mongoose.Schema.Types.Number,
+    required: true
+  },
+  pages: {
+    type: mongoose.Schema.Types.Number,
+    required: true
+  }
+}, {
+  versionKey: false
+})
+
+export const Book = mongoose.model('books', bookSchema)
